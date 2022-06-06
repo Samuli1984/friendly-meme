f8bfd59ed0868008ce50679fe012c1a2992bc5f5
Samuli1984 <samulipulkkinen1984@gmail.com>
2022-06-06 14:15:29 +0300

    Vaihda tekstiä sovellus kokonaisuudessaan.

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tervehdysTeksti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tervehdysTeksti = findViewById(R.id.tvTervehdys);
+    }
+
+    public void vaihdaTeksti(View view)
+    {
+        tervehdysTeksti.setText("Hei maailma");
     }
 }
